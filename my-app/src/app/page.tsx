4828899bc2b2ed238192af25188c56c0fb30d522
Cdric2Leroy <cedric.leroy.psycho@gmail.com>
2024-07-15 22:47:12 +0200

    test dark mode

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,6 +1,9 @@
+import {ModeToggle} from "@/components/Modetoggle";
+
 export default function Home() {
   return (
       <>
+        <ModeToggle></ModeToggle>
         <h1>Hello World</h1>
       </>
   );
