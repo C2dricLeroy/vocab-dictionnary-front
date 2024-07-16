07aa0e9d41e14dbc1beb08c19e4d33aa219603a7
Cdric2Leroy <cedric.leroy.psycho@gmail.com>
2024-07-16 21:58:48 +0200

    add settings page

---
+++ my-app/src/app/settings/page.tsx
@@ -0,0 +1,10 @@
+import {ModeToggle} from "@/components/Modetoggle";
+
+export default function Settings() {
+    return (
+        <>
+            <ModeToggle></ModeToggle>
+            <h1>Settings</h1>
+        </>
+    );
+}
