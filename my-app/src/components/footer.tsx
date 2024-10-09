b73b1d546b834a62a1e85a45d83ea8b650b1e0f2
celer <cedric.leroy@smile.fr>
2024-10-09 15:58:04 +0200

    add responsivity to footer

--- my-app/src/components/footer.tsx
+++ my-app/src/components/footer.tsx
@@ -3,33 +3,33 @@ export default function Footer() {
     <footer className="bg-gray-200 text-gray-900 py-8 shadow-[8px_8px_16px_#d1d9e6,_-8px_-8px_16px_#ffffff] dark:bg-gray-800 dark:text-white dark:shadow-[inset_1px_1px_2px_#2e2e2e,_inset_-1px_-1px_2px_#3f3f3f]">
       <div className="container mx-auto px-4">
         <div className="flex flex-wrap justify-between">
-
-          <div className="w-full md:w-1/3 mb-6">
+          
+          <div className="w-full sm:w-1/2 lg:w-1/3 mb-6">
             <h2 className="text-lg font-semibold mb-4">About Us</h2>
             <p className="text-gray-600 dark:text-gray-400">
               Simply learn by cumulating knowledge.
             </p>
           </div>
 
-          <div className="w-full md:w-1/3 mb-6">
+          <div className="w-full sm:w-1/2 lg:w-1/3 mb-6">
             <h2 className="text-lg font-semibold mb-4">Quick Links</h2>
             <ul className="text-gray-600 dark:text-gray-400">
               <li className="mb-2">
                 <a href="#" className="hover:underline hover:text-gray-800 dark:hover:text-gray-300">Home</a>
               </li>
               <li className="mb-2">
-                <a href="/about" className="hover:underline hover:text-gray-800 dark:hover:text-gray-300">About</a>
+                <a href="#" className="hover:underline hover:text-gray-800 dark:hover:text-gray-300">About</a>
               </li>
               <li className="mb-2">
-                <a href="/pricing" className="hover:underline hover:text-gray-800 dark:hover:text-gray-300">Pricing</a>
+                <a href="#" className="hover:underline hover:text-gray-800 dark:hover:text-gray-300">Pricing</a>
               </li>
               <li className="mb-2">
-                <a href="/contact" className="hover:underline hover:text-gray-800 dark:hover:text-gray-300">Contact</a>
+                <a href="#" className="hover:underline hover:text-gray-800 dark:hover:text-gray-300">Contact</a>
               </li>
             </ul>
           </div>
 
-          <div className="w-full md:w-1/3 mb-6">
+          <div className="w-full sm:w-1/2 lg:w-1/3 mb-6">
             <h2 className="text-lg font-semibold mb-4">Contact Us</h2>
             <p className="text-gray-600 dark:text-gray-400">Email: contact@lexilearn.eu</p>
             <div className="flex space-x-4 mt-4">
