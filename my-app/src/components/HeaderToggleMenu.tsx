ad24e18296381fabd2775ef8685f5f1fac8f02db
celer <cedric.leroy@smile.fr>
2024-12-12 07:16:28 +0100

    [FIX] adjust button size

--- my-app/src/components/HeaderToggleMenu.tsx
+++ my-app/src/components/HeaderToggleMenu.tsx
@@ -10,21 +10,21 @@ export default function HeaderToggleMenu(context: any) {
     const t = useTranslations('WelcomeHeader');
 
     return (
-        <div className="flex flex-col mt-4 space-y-4 w-full items-center">
-            <Button size="lg" variant="outline" className="w-3/4">
+        <div className="flex flex-col mt-4 space-y-4 items-center">
+            <Button size="sm" variant="outline" className="w-3/4 h-[50px]">
                 {/* @ts-expect-error on Link href*/}
                 <Link href="/pricing">{t("Pricing")}</Link>
             </Button>
-            <Button size="lg" variant="outline" className="w-3/4">
+            <Button size="sm" variant="outline" className="w-3/4 h-[50px]">
                 {/* @ts-expect-error on Link href*/}
                 <Link href="/about">{t("About")}</Link>
             </Button>
             {context.isAuthenticated ? (
-                <Button size="lg" onClick={context.logout} className="w-3/4">
+                <Button size="sm" onClick={context.logout} className="w-3/4 h-[50px]">
                     {t("Logout")}
                 </Button>
             ) : (
-                <Button size="lg" className="w-3/4">
+                <Button size="sm" className="w-3/4 h-[50px]">
                     {/* @ts-expect-error on Link href*/}
                     <Link href="/signin">{t("Login")}</Link>
                 </Button>
