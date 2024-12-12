aa10aa0116ddb4e67b3c73711b19373f3f702a8e
celer <cedric.leroy@smile.fr>
2024-12-12 07:16:28 +0100

    [FIX] adjust button size

--- my-app/src/components/ui/button.tsx
+++ my-app/src/components/ui/button.tsx
@@ -26,6 +26,7 @@ const buttonVariants = cva(
         sm: "h-8 rounded-md px-3 text-xs",
         lg: "h-10 rounded-md px-8",
         icon: "h-9 w-9",
+        responsive: "h-8 px-3 text-xs md:h-9 md:px-4 lg:h-10 lg:px-6"
       },
     },
     defaultVariants: {
