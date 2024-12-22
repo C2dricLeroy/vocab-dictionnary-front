0dc1cf53eb5bda1612bd68097657302d2c160679
C2dricLeroy <cedric.leroy.psycho@gmail.com>
2024-12-22 17:46:26 +0100

    [Clean up dashboard code + impelemnt profile basic routing

--- my-app/src/components/dashboard/DashboardProfile.tsx
+++ my-app/src/components/dashboard/DashboardProfile.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
+import { useTranslations } from "next-intl";
+import { Link } from "@/i18n/routing";
 
 export const DashboardProfile: React.FC = () => {
+
+    const t = useTranslations("Dashboard");
+
     return (
         <Card className="bg-white dark:bg-gray-800">
             <CardHeader>
@@ -12,7 +17,10 @@ export const DashboardProfile: React.FC = () => {
                 <div className="space-y-4">
                     <p>Nom : John Doe</p>
                     <p>Email : john.doe@example.com</p>
-                    <Button variant="default">Modifier Profil</Button>
+                    <Button variant="default">
+                        {/* @ts-expect-error on Link href*/}
+                        <Link href="/profile">{t("Modify Profile")}</Link>
+                    </Button>
                 </div>
             </CardContent>
         </Card>
